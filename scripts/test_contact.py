bd2dc5134b977648863faaa61cfadcbb0a855d11
HodorandLexi <jmj89421844@163.com>
2018-11-12 15:04:14 +0800

    更改

--- scripts/test_contact.py
+++ scripts/test_contact.py
@@ -8,23 +8,13 @@ from base.base_analyze import analyze_file
 
 class TestContact:
 
-    def setup(self):
-        self.driver = init_driver()
-        self.page = Page(self.driver)
+    def test_c1(self):
+        assert 1
 
-    def teardown(self):
-        time.sleep(5)
-        self.driver.quit()
-
-    @pytest.mark.parametrize("args", analyze_file("contact_data.yml", "test_add_contact"))
-    def test_add_contact(self, args):
-        name = args["name"]
-        phone = args["phone"]
-
-        self.page.contact.click_add_contact()
-        self.page.new_contact.input_name(name)
-        self.page.new_contact.input_phone(phone)
-        self.page.new_contact.click_back()
-        assert name == self.page.saved_contect.get_name_title_text()
+    def test_c2(self):
+        assert 1
+		
+	def test_c3(self):
+        assert 1
 
 
